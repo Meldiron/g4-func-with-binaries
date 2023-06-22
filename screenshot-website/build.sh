6bfe2c5f52137899dba32b508623ce74a3e67e57
Matej Bačo <matejbaco2000@gmail.com>
2023-06-22 10:34:55 +0200

    Implement build

--- screenshot-website/build.sh
+++ screenshot-website/build.sh
@@ -1,2 +1,12 @@
-echo "Custom Build"
-echo "Rebuild"
+echo "Downloading Chromium"
+
+CHROME_BIN=/usr/bin/chromium-browser
+
+echo @edge http://nl.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories
+echo @edge http://nl.alpinelinux.org/alpine/edge/main >> /etc/apk/repositories
+
+apk add --no-cache chromium@edge nss@edge
+
+wc -c /usr/bin/chromium-browser
+
+echo "Chromium downloaded successfully"
