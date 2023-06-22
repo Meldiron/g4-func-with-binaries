7b4071b95c127e7a375bf8f81e6e9cec49692bcf
Matej Bačo <matejbaco2000@gmail.com>
2023-06-22 10:26:14 +0200

    Add base of screenshot func

---
+++ screenshot-website/build.sh
@@ -0,0 +1,1 @@
+echo "Custom Build"
---
+++ screenshot-website/index.js
@@ -0,0 +1,30 @@
+const puppeteer = require("puppeteer");
+
+module.exports = async ({ res }) => {
+  const browser = await puppeteer.launch({
+    executablePath: process.env.CHROME_BIN || null,
+    args: ["--no-sandbox", "--headless", "--disable-gpu"],
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 720 });
+
+  const website_url = "https://appwrite.io/";
+  await page.goto(website_url, { waitUntil: "networkidle0" });
+
+  const imge = await page.screenshot({
+    type: "jpeg",
+    quality: 100,
+    clip: {
+      x: 0,
+      y: 70,
+      width: 640,
+      height: 360,
+    },
+    omitBackground: true,
+  });
+
+  await browser.close();
+
+  return res.send(image);
+};
